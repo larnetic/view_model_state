14c6f0c25fdff1ca179cba6d2193383b00315854
Lars vom Bruch <lrsvombruch@gmail.com>
2025-08-30 19:02:20 +0200

    add onUpdate callback to viewmodel

--- test/view_model_test.dart
+++ test/view_model_test.dart
@@ -9,6 +9,15 @@ final stream = Stream<int>.empty();
 
 class TestViewModel extends ViewModel {
   late final streamState = createStateFromStream(controller.stream, 0);
+  late final mutableState = createMutableState(0);
+
+  var onUpdateCallCount = 0;
+
+  @override
+  void onUpdate() {
+    onUpdateCallCount++;
+    super.onUpdate();
+  }
 }
 
 void main() {
@@ -38,6 +47,85 @@ void main() {
       final viewModel = TestViewModel();
       viewModel.dispose();
       expect(() => viewModel.dispose(), throwsA(isA<Error>()));
+    });
+  });
+
+  group("Test self update", () {
+    test("onUpdate is called on notifyListeners", () {
+      final viewModel = TestViewModel();
+      expect(viewModel.onUpdateCallCount, 0);
+      viewModel.notifyListeners();
+      expect(viewModel.onUpdateCallCount, 1);
+      viewModel.notifyListeners();
+      expect(viewModel.onUpdateCallCount, 2);
+    });
+
+    test("onUpdate is called on mutable state mutation", () {
+      final viewModel = TestViewModel();
+      expect(viewModel.onUpdateCallCount, 0);
+      viewModel.mutableState.value = 1;
+      expect(viewModel.onUpdateCallCount, 1);
+    });
+
+    test("onUpdate is called on ValueNotifier change", () {
+      final viewModel = TestViewModel();
+      final notifier = ValueNotifier<int>(0);
+      final state = viewModel.createStateFromValueNotifier(notifier);
+      expect(state.value, 0);
+      expect(viewModel.onUpdateCallCount, 0);
+      notifier.value = 2;
+      expect(viewModel.onUpdateCallCount, 1);
+    });
+
+    test("onUpdate is called on Stream change", () async {
+      final viewModel = TestViewModel();
+      final stream = Stream<int>.fromIterable([1, 2, 3]);
+      final state = viewModel.createStateFromStream(stream, 0);
+      expect(state.value, 0);
+      expect(viewModel.onUpdateCallCount, 0);
+      await Future.delayed(Duration.zero);
+      expect(state.value, 3);
+      expect(viewModel.onUpdateCallCount, 3);
+    });
+
+    test("onUpdate is called on future change", () async {
+      final viewModel = TestViewModel();
+      final future = Future<int>.delayed(Duration.zero, () => 42);
+      final state = viewModel.createStateFromFuture(future, 0);
+      expect(state.value, 0);
+      expect(viewModel.onUpdateCallCount, 0);
+      await Future.delayed(Duration.zero);
+      expect(state.value, 42);
+      expect(viewModel.onUpdateCallCount, 1);
+    });
+
+    test("onUpdate is called on multiple state changes", () async {
+      final viewModel = TestViewModel();
+      final stream = Stream<int>.fromIterable([1, 2, 3]);
+      final future = Future<int>.delayed(Duration.zero, () => 42);
+      final notifier = ValueNotifier<int>(0);
+      final state1 = viewModel.createStateFromStream(stream, 0);
+      final state2 = viewModel.createStateFromFuture(future, 0);
+      final state3 = viewModel.createStateFromValueNotifier(notifier);
+      expect(state1.value, 0);
+      expect(state2.value, 0);
+      expect(state3.value, 0);
+      expect(viewModel.onUpdateCallCount, 0);
+      await Future.delayed(Duration.zero);
+      expect(state1.value, 3);
+      expect(state2.value, 42);
+      expect(state3.value, 0);
+      expect(viewModel.onUpdateCallCount, 4);
+      notifier.value = 5;
+      expect(state3.value, 5);
+      expect(viewModel.onUpdateCallCount, 5);
+    });
+
+    test("onUpdate is not called on dispose", () {
+      final viewModel = TestViewModel();
+      expect(viewModel.onUpdateCallCount, 0);
+      viewModel.dispose();
+      expect(viewModel.onUpdateCallCount, 0);
     });
   });
 }
